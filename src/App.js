f26f20be1d6fef56d805c67f0b3c7c7c526c5b2f
deba-69 <debasishpanigrahi200@gmail.com>
2024-02-11 17:29:22 +0530

    First project commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Home from './Components/pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import Services from './Components/pages/Services';
+import SignUp from './Components/pages/SignUp';
+import Destination from './Components/pages/Destination';
+import ScrollToTop from './Components/ScrollToTop';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='App'>
+       <Navbar />
+      <ScrollToTop>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/services' exact element={<Services />} />
+          <Route path='/sign-up' exact element={<SignUp />} />
+          <Route path='/services/activity' element={<Destination />} />
+        </Routes>
+      </ScrollToTop>
     </div>
   );
 }
